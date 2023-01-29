e679e23e6825ea6682e0ea1134a5cac3b2a38867
Aman Abdirazak <amanabdirizak@icloud.com>
2023-01-29 17:17:41 -0600

    MovieForm is set up to make searches, will work on saga axios next

--- src/components/MovieForm/MovieForm.js
+++ src/components/MovieForm/MovieForm.js
@@ -1,10 +1,45 @@
-
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import MovieFormItem from '../MovieFormItem/MovieFormItem';
+import './MovieForm.css'
+//mui components
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
 
 function MovieForm() {
 
+    //assinging inputs a piece of state
+    const [search, setSearch] = useState('');
+    const dispatch = useDispatch();
+
+    const searchMovies = (event) => {
+        event.preventDefault();
+        dispatch({
+            type: 'SAGA/SEARCH_MOVIES',
+            payload: search
+        })
+        //clear input
+        setSearch('');
+    }
+
     return(
         <>
-            <input></input>
+            <form onSubmit={searchMovies}>
+                <TextField 
+                    value={search}
+                    label="Your search awaits!"
+                    varient="standard"
+                    sx={{backgroundColor: '#E8F7EE', color: '#663F46'}}
+                    onChange={(event) => setSearch(event.target.value)}
+                />
+                <Button
+                    varient="contained"
+                    type='submit'
+                    sx={{backgroundColor: '#E8F7EE', color: '#663F46', margin: '10px'}}
+                >
+                    Search
+                </Button>
+            </form>
         </>
     )
 }
---
+++ src/components/MovieFormItem/MovieFormItem.js
@@ -0,0 +1,13 @@
+
+
+function MovieFormItem() {
+    
+    
+    return(
+        <>
+            <h3>whats up</h3>
+        </>
+    )
+}
+
+export default MovieFormItem
