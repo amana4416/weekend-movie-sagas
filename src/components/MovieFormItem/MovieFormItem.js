7d1f605f4e0120c9c228ecf47c5abc3f6501e8c9
Aman Abdirazak <amanabdirizak@icloud.com>
2023-01-29 18:39:26 -0600

    styled MovieFormItem

--- src/components/MovieFormItem/MovieFormItem.js
+++ src/components/MovieFormItem/MovieFormItem.js
@@ -1,6 +1,7 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import './MovieFormItem.css';
 //mui components
 import Paper from '@mui/material/Paper';
 
@@ -16,15 +17,14 @@ function MovieFormItem({results}) {
     
     return (
         <>
-            {/* <Paper 
+            <Paper 
                 className="resultBackground"
                 elevation={3}
-                sx={{backgroundColor: '#B8C4BB', margin: '30px', height: '390px', width: '300px'}}
-            > */}
-                <h3>{results.original_title}</h3>
+                sx={{backgroundColor: '#B8C4BB', margin: '30px', height: '390px', width: '400px'}}
+            >
+                <h4>{results.original_title}</h4>
                 <img src={`https://image.tmdb.org/t/p/original/${results.poster_path}`} alt={results.title} />
-
-            {/* </Paper> */}
+            </Paper>
         </>
     )
 }
