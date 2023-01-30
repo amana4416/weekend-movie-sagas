e961c24386bf6cb1247a3f497de8cd7720966e93
Aman Abdirazak <amanabdirizak@icloud.com>
2023-01-29 21:54:27 -0600

    wrote server side get route for favorites

--- server/routes/favorites.router.js
+++ server/routes/favorites.router.js
@@ -2,7 +2,23 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool')
 
-//post route to get favorite movies
+//get route to display favorite movies
+router.get('/', (req, res) => {
+  const sqlQuery = `
+    SELECT * FROM favorites
+      ORDER BY "title" ASC;
+  `;
+  pool.query(sqlQuery)
+  .then((response) => {
+    res.send(response.rows);
+  })
+  .catch((error) => {
+    console.log('error in /api/favorites GET', error);
+    res.sendStatus(500);
+  })
+})
+
+//post route to add favorite movies
 router.post('/', (req, res) => {
     console.log('HELLO NEW FAV',req.body)
     const newFavorite = req.body;
